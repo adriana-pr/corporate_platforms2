1b8044d1344448f02525760a1038a64bdc3c00a4
adriana-pr <94687330+adriana-pr@users.noreply.github.com>
2023-10-28 12:45:19 +0300

    Add files via upload

---
+++ lab2/lab2/lab2/Models/Amountofbill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab2.Models;
+
+public partial class Amountofbill
+{
+    public int Id { get; set; }
+
+    public int ContributorId { get; set; }
+
+    public double Amount { get; set; }
+
+    public virtual Contributor Contributor { get; set; } = null!;
+}
---
+++ lab2/lab2/lab2/Program.cs
@@ -0,0 +1,36 @@
+﻿using lab2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        using (BankDepositsContext db = new BankDepositsContext())
+        {
+            var query = db.Contributors
+                    .Join(db.Bankdeposits, c => c.Id, bd => bd.ContributorId, (c, bd) => new { c.FirstName, c.LastName, c.Patronymic })
+                    .GroupBy(g => new { g.FirstName, g.LastName, g.Patronymic })
+                    .Where(g => g.Count() > 1)
+                    .Select(g => new
+                    {
+                        g.Key.FirstName,
+                        g.Key.LastName,
+                        g.Key.Patronymic,
+                        Count = g.Count()
+                    })
+                    .ToList();
+
+            foreach (var item in query)
+            {
+                Console.WriteLine($"Прізвище: {item.LastName}, Ім'я: {item.FirstName}, По батькові: {item.Patronymic}, Кількість: {item.Count}");
+            }
+        }
+    }
+}
+
+
+
+
